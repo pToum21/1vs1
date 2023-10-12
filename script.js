42556f1eb3075733c5491ce74dcde36d2959ad52
toum21 <pey2moo1@gmail.com>
2023-10-11 21:57:59 -0700

    began OOP in js and did basic html layout

---
+++ script.js
@@ -0,0 +1,12 @@
+function characters(name, health, classBuild, bio) {
+    this.name = name
+    this.health = health;
+    this.classBuild = classBuild;
+    this.bio = bio;
+    //    this.stats = stats
+};
+
+const character1 = new characters('Bricko', 100, 'big hoss', 'a being made of bricks whose pure talent is defense');
+const character2 = new characters('JazzLord', 100, 'sound waves', 'a small figther whoes saxsophone can blow out oppents ear drums leaving them knocked out');
+const character3 = new characters('Sweet', 100, 'elusive', 'skinny but quick, sweet is hard for an oppent to hit in combat');
+const character4 = new characters('Big Toe', 100, 'God Tier', 'Big Toe is litterally a Big Toe, allow him to get one hit and its an automatic win');
