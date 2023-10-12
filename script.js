b99c4b3e71667dfd93650f76ffad546fe5b59e1e
toum21 <pey2moo1@gmail.com>
2023-10-11 22:13:37 -0700

    tested a character taking damage

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ function characters(name, health, classBuild, bio) {
 };
 
 const character1 = new characters('Bricko', 100, 'big hoss', 'a being made of bricks whose pure talent is defense');
+character1.damageTaken(40)
+console.log(character1)
 const character2 = new characters('JazzLord', 100, 'sound waves', 'a small figther whoes saxsophone can blow out oppents ear drums leaving them knocked out');
 const character3 = new characters('Sweet', 100, 'elusive', 'skinny but quick, sweet is hard for an oppent to hit in combat');
 const character4 = new characters('Big Toe', 100, 'God Tier', 'Big Toe is litterally a Big Toe, allow him to get one hit and its an automatic win');
