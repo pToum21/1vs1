3fb09ec2554b35fd6c37ecdf12892a00bd0a7060
toum21 <pey2moo1@gmail.com>
2023-10-11 22:09:45 -0700

    added small updates to js

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+var startGame = document.querySelector('#select')
+
+
+
+
 function characters(name, health, classBuild, bio) {
-    this.name = name
+
+    this.name = name;
+
     this.health = health;
+    this.damageTaken = function (amount) {
+        this.health -= amount;
+    }
+
     this.classBuild = classBuild;
     this.bio = bio;
-    //    this.stats = stats
 };
 
 const character1 = new characters('Bricko', 100, 'big hoss', 'a being made of bricks whose pure talent is defense');
 const character2 = new characters('JazzLord', 100, 'sound waves', 'a small figther whoes saxsophone can blow out oppents ear drums leaving them knocked out');
 const character3 = new characters('Sweet', 100, 'elusive', 'skinny but quick, sweet is hard for an oppent to hit in combat');
 const character4 = new characters('Big Toe', 100, 'God Tier', 'Big Toe is litterally a Big Toe, allow him to get one hit and its an automatic win');
+
+
+
+startGame.addEventListener('click', function (event) {
+    event.preventDefault()
+
+    selectCharacters();
+})
+
+
