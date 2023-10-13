6f711fe03b3aede5e2b6598ba72904727ca049fb
diarmuid-murphy <diarmuid.com@gmail.com>
2023-10-13 13:34:47 -0700

    Refactored object constructor to use uppercase character, added a Game constructor.

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 var startGame = document.querySelector('#start-btn')
 var selectedCharacter;
 
+function Game() {
+    // implement these functions
+    this.start = function () { };
+    this.quit = function () { };
+    this.printStats = function () { };
+    this.promptAction = function () { };
+    this.turn = 0;
+    this.selectedCharacters = [];
+}
 
-
-function characters(name, health, classBuild, bio) {
+function Character(name, health, classBuild, bio) {
 
     this.name = name;
 
@@ -16,12 +24,12 @@ function characters(name, health, classBuild, bio) {
     this.bio = bio;
 };
 
-const character1 = new characters('Bricko', 100, 'big hoss', 'a being made of bricks whose pure talent is defense');
+const character1 = new Character('Bricko', 100, 'big hoss', 'a being made of bricks whose pure talent is defense');
 character1.damageTaken(40)
 console.log(character1)
-const character2 = new characters('JazzLord', 100, 'sound waves', 'a small figther whoes saxsophone can blow out oppents ear drums leaving them knocked out');
-const character3 = new characters('Sweet', 100, 'elusive', 'skinny but quick, sweet is hard for an oppent to hit in combat');
-const character4 = new characters('Big Toe', 100, 'God Tier', 'Big Toe is litterally a Big Toe, allow him to get one hit and its an automatic win');
+const character2 = new Character('JazzLord', 100, 'sound waves', 'a small figther whoes saxsophone can blow out oppents ear drums leaving them knocked out');
+const character3 = new Character('Sweet', 100, 'elusive', 'skinny but quick, sweet is hard for an oppent to hit in combat');
+const character4 = new Character('Big Toe', 100, 'God Tier', 'Big Toe is litterally a Big Toe, allow him to get one hit and its an automatic win');
 
 function selectCharacter() {
     selectedCharacter = character2;
@@ -34,24 +42,25 @@ startGame.addEventListener('click', function (event) {
         alert('Please select a Character before satrting the game')
         return;
     }
-function chooseCharacter() {
-    let characterDiv = document.getElementById('character-selection')
-    let characters = [character1, character2, character3, character4];
-    characters.forEach(function (character) {
-        let characterSelectionEl = document.createElement('div');
-        characterSelectionEl.className = 'character';
-        characterSelectionEl.innerHTML = `
+    function chooseCharacter() {
+        let characterDiv = document.getElementById('character-selection')
+        let characters = [character1, character2, character3, character4];
+        characters.forEach(function (character) {
+            let characterSelectionEl = document.createElement('div');
+            characterSelectionEl.className = 'character';
+            characterSelectionEl.innerHTML = `
         <h2>${character.name}</h2>
         <p>${character.classBuild}</p>
     `;
-    characterSelectionEl.addEventListener('click', function(){
-        selectCharacter(character);
-    })
-    characterSelectionEl.appendchild(characterDiv)
-    })
-}
+            characterSelectionEl.addEventListener('click', function () {
+                selectCharacter(character);
+            })
+            characterSelectionEl.appendchild(characterDiv)
+        })
+    }
     chooseCharacter()
-    
+
 })
 
-
+const game = new Game();
+game.start();
