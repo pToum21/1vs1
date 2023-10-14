79766462549dec4e6009d610770ccc79a62c4953
toum21 <pey2moo1@gmail.com>
2023-10-14 13:39:00 -0700

    added node as well as command line prompts for the game

--- script.js
+++ script.js
@@ -1,66 +1,92 @@
-var startGame = document.querySelector('#start-btn')
+const inquirer = require('inquirer')
+
+// var startGame = document.querySelector('#start-btn')
 var selectedCharacter;
 
 function Game() {
     // implement these functions
+    this.turn = 0;
+    this.selectedCharacters = [];
+
     this.start = function () { };
     this.quit = function () { };
     this.printStats = function () { };
-    this.promptAction = function () { };
-    this.turn = 0;
-    this.selectedCharacters = [];
+    this.createCharacter = function () {
+        inquirer
+            .prompt([
+                {
+                    name: 'name',
+                    message: 'What is your characters name?',
+                    type: 'input'
+                },
+                
+                {
+                    name: 'classBuild',
+                    message: 'What is you characters Class/Type?',
+                    type: 'input'
+                },
+                {
+                    name: 'bio',
+                    message: 'Describe your charcter',
+                    type: 'input'
+                },
+            ])
+            .then(function (answers) {
+                console.log(answers);
+
+                const you = new Character(
+                    answers.name,
+                    Math.ceil(Math.random() * 100),
+                    Math.ceil(Math.random() * 60),
+                    answers.classBuild,
+                    answers.bio
+                );
+
+                const computer = new Character(
+                    "Enemy",
+                    Math.ceil(Math.random() * 100),
+                    Math.ceil(Math.random() * 60),
+                    "Enemy Class",
+                    "From the depths of hell"
+                );
+
+                console.log(you);
+                console.log(computer);
+
+                you.dealDamage(computer);
+                computer.dealDamage(you);
+
+                console.log(you);
+                console.log(computer);
+            });
+    }
 }
 
-function Character(name, health, classBuild, bio) {
+function Character(name, health, attack, classBuild, bio) {
 
     this.name = name;
-
+    this.attack = attack;
     this.health = health;
     this.damageTaken = function (amount) {
         this.health -= amount;
-    }
 
+        if (this.health <= 0) {
+            console.log(this.name + " has been defeated!");
+            game.end
+        }
+    }
+    this.dealDamage = function (character) {
+        character.damageTaken(this.attack);
+    };
     this.classBuild = classBuild;
     this.bio = bio;
 };
 
-const character1 = new Character('Bricko', 100, 'big hoss', 'a being made of bricks whose pure talent is defense');
-character1.damageTaken(40)
-console.log(character1)
-const character2 = new Character('JazzLord', 100, 'sound waves', 'a small figther whoes saxsophone can blow out oppents ear drums leaving them knocked out');
-const character3 = new Character('Sweet', 100, 'elusive', 'skinny but quick, sweet is hard for an oppent to hit in combat');
-const character4 = new Character('Big Toe', 100, 'God Tier', 'Big Toe is litterally a Big Toe, allow him to get one hit and its an automatic win');
-
-function selectCharacter() {
-    selectedCharacter = character2;
-    console.log('Selected character:', selectedCharacter);
-}
-
-startGame.addEventListener('click', function (event) {
-    event.preventDefault()
-    if (!selectedCharacter) {
-        alert('Please select a Character before satrting the game')
-        return;
-    }
-    function chooseCharacter() {
-        let characterDiv = document.getElementById('character-selection')
-        let characters = [character1, character2, character3, character4];
-        characters.forEach(function (character) {
-            let characterSelectionEl = document.createElement('div');
-            characterSelectionEl.className = 'character';
-            characterSelectionEl.innerHTML = `
-        <h2>${character.name}</h2>
-        <p>${character.classBuild}</p>
-    `;
-            characterSelectionEl.addEventListener('click', function () {
-                selectCharacter(character);
-            })
-            characterSelectionEl.appendchild(characterDiv)
-        })
-    }
-    chooseCharacter()
-
-})
-
 const game = new Game();
-game.start();
+game.createCharacter();
+
+// console.log(game.selectedCharacters);
+
+// npm init -y
+
+// npm install inquirer@8.2.4
